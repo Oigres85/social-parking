8b49ce2df44625aa01d961273f59cd9a96ca3eee
Sergio Garofalo <sergio.garofalo@siigep.tech>
2026-01-29 09:02:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6366379090-251c5",
+  "appId": "1:898470092455:web:525f0bbee99db943660582",
+  "apiKey": "AIzaSyCcaJI-Ab6wKbk8_C9BckHf5JXblK8aLQs",
+  "authDomain": "studio-6366379090-251c5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "898470092455"
+};
